4ea1a7a3cd128ec9152078ec80d0a183e573f332
shiori saitou <shi.o.rin.5082@gmail.com>
2024-06-08 14:32:04 +0000

    [Add]users,foods,homes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    get 'ideal' => 'homes#ideal'
+    resources :users, only: [:show, :edit, :update]
+    resources :foods, only: [:new, :index, :show, :edit, :create, :update, :destroy]
+  end
   
+  amespace :admin do
+    root to: 'homes#top'
+    resources :foods, only: [:index, :show, :destroy]
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+
   devise_for :user, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
