c186f2e282cf4f56b776f4e8ac6c5e1e600cc2af
shiori saitou <shi.o.rin.5082@gmail.com>
2024-06-09 07:58:37 +0000

    [Add]controllers作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
   
   
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'foods/index'
+    get 'foods/show'
+  end
+  namespace :admin do
+    get 'genres/index'
+  end
+  namespace :public do
+    get 'rooms/index'
+    get 'rooms/show'
+  end
+  namespace :public do
+    get 'relationships/followers'
+    get 'relationships/followings'
+  end
   devise_for :user, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
