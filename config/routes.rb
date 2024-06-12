82bf8e9cd4981e83947fabad600fcdd991948364
shiori saitou <shi.o.rin.5082@gmail.com>
2024-06-12 14:56:11 +0000

    [コメント化解除]

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,30 @@
 Rails.application.routes.draw do
 
-  # scope module: :public do
-  #   root to: 'homes#top'
-  #   get 'about' => 'homes#about'
-  #   get 'ideal' => 'homes#ideal'
-  #   resources :users, only: [:show, :edit, :update] do
-  #     resources :rooms, only: [:create, :show, :index] do
-  #       resources :dms, only: [:create, :destroy]
-  #     end
-  #     resource :relationships, only: [:create, :destroy]
-  #   	get "followings" => "relationships#followings", as: "followings"
-  #   	get "followers" => "relationships#followers", as: "followers"
-  #   end
-  #   resources :foods, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
-  #     resources :comments, only: [:create, :destroy]
-  #     resource :favorites, only: [:create, :destroy]
-  #   end
-  #   resources :relationships, only: [:followed, :follower, :create, :destroy]
-  # end
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    get 'ideal' => 'homes#ideal'
+    resources :users, only: [:show, :edit, :update] do
+      resources :rooms, only: [:create, :show, :index] do
+        resources :dms, only: [:create, :destroy]
+      end
+      resource :relationships, only: [:create, :destroy]
+    	get "followings" => "relationships#followings", as: "followings"
+    	get "followers" => "relationships#followers", as: "followers"
+    end
+    resources :foods, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
+      resources :comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
+    resources :relationships, only: [:followed, :follower, :create, :destroy]
+  end
 
-  # amespace :admin do
-  #   root to: 'homes#top'
-  #   resources :foods, only: [:index, :show, :destroy]
-  #   resources :users, only: [:index, :show, :edit, :update]
-  #   resources :genres, only: [:index, :create, :destroy]
-  # end
+  amespace :admin do
+    root to: 'homes#top'
+    resources :foods, only: [:index, :show, :destroy]
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :destroy]
+  end
 
 
   devise_for :user, skip: [:passwords], controllers: {
