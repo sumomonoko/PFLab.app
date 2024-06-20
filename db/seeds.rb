d782f852806324b0a77db412f4ef8869abc7794d
shiori saitou <shi.o.rin.5082@gmail.com>
2024-06-20 14:58:01 +0000

    [Add]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.find_or_create_by!(email: 'foodlab@gmail') do |admin|
+  admin.password = "foodlab1321"
+end
+
+Genre.find_or_create_by!(name: '朝食')
+
+Genre.find_or_create_by!(name: '昼食')
+
+Genre.find_or_create_by!(name: '夕食')
+
+Genre.find_or_create_by!(name: '間食')
+
+Genre.find_or_create_by!(name: 'お弁当')
+
+Genre.find_or_create_by!(name: 'デザート')
+
+Genre.find_or_create_by!(name: 'おやつ')
