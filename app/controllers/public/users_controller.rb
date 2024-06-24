8c61cdd2d7496e1253510f16cb466dee87e28eb7
shiori saitou <shi.o.rin.5082@gmail.com>
2024-06-24 14:58:41 +0000

    [Add]退会機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -13,7 +13,20 @@ class Public::UsersController < ApplicationController
 
   def update
     @user = current_user
+  end
 
+  def check
+    @user = current_user
+  end
+
+  def withdraw
+    @user = User.find(current_user.id)
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @user.update(is_active: false)
+    #セッション情報を全て削除
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました。"
+    redirect_to root_path
   end
 
   private
